81a821b7d1d61496ba336165dad4909a15742a54
bkm <ball.kamol@gmail.com>
2022-03-25 14:48:41 +0700

    add test rs485, range ID 0-50(0x00-0x32)

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def read_holding(slave_address):
     timeout = time.perf_counter()
     status_timeout = 0
     while (ser.inWaiting()) == 0:  # Or: while ser.inWaiting():
-        if (time.perf_counter() - timeout) > 1:
+        if (time.perf_counter() - timeout) > 5:
             print('RS-485 Timeout')
             status_timeout = 1
             break
@@ -422,6 +422,7 @@ print('Check Device on Network 1-50')
 bus_id_list = []
 for i in range(0x00, 0x32):
     print(i)
+
     if read_holding(i) != 0:
         print('bus ID:' + str(i) + ' Available')
         bus_id_list.append(i)
